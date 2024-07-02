2ef08bcab2afcb0b981b44dce463fa4f548f5757
naturedamends <120284608+naturedamends@users.noreply.github.com>
2024-07-02 05:50:02 +0100

    save

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@
 
 # Create symlinks
 ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -nfs $HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme
